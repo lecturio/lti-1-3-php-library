b1dd212dca8b4b0e0b254920035a8eacb8d92408
Lyubomir Lyubenov <lyubetrix@gmail.com>
2021-03-29 14:37:15 +0300

    Removed debug var_dump() in catch - causes browser crash

--- src/lti/LTI_Message_Launch.php
+++ src/lti/LTI_Message_Launch.php
@@ -309,7 +309,8 @@ class LTI_Message_Launch {
         try {
             JWT::decode($this->request['id_token'], $public_key['key'], array('RS256'));
         } catch(\Exception $e) {
-            var_dump($e);
+            // Removed dump - object is too large - browser freeze
+            //var_dump($e);
             // Error validating signature.
             throw new LTI_Exception("Invalid signature on id_token", 1);
         }
